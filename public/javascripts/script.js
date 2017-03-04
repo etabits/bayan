ce01bc0c46403e777db0313a554519081f8240c7
Hasan Arous <arous.hasan@gmail.com>
2017-03-04 09:24:11 +0200

    More updates, nested things and more!

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,9 +11,20 @@ Bayan.UI.arrayDel = function (btn) {
 
 Bayan.UI.arrayAdd = function (btn) {
   var container = $(btn).parents('.bayan-sub-container').first()
-  var template = container.find('script')
-  var number = template.attr('data-next-number')
-  var html = template.text().replace(/{NUM}/g, number)
-  console.log(html)
-  $(html).attr('class', 'bayan-array-element').insertBefore(container.find('.bayan-add-array-element'))
+  var template = container.find('.bayan-template').first()
+  if (template.length>1) {
+    ddd()
+  }
+  var number = parseInt(template.attr('data-next-number'))
+  template.attr('data-next-number', number+1)
+  var html = template.attr('data-template-string').replace(/{NUM}/g, number)
+  var addTo = container.find('.bayan-add-array-element').last()
+  console.log(html, addTo)
+  $(html).attr('class', 'bayan-array-element').insertBefore(addTo)
 }
+
+$('.bayan-template').toArray().reverse().forEach(function(tpl) {
+  var innerHTML = tpl.innerHTML;
+  tpl.innerHTML = ''
+  $(tpl).attr('data-template-string', innerHTML)
+})

330c9d17bce3c55c3c37780111ac015fab97a85c
Hasan Arous <arous.hasan@gmail.com>
2017-03-04 19:21:48 +0200

    selects, chips and more

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,7 +4,6 @@ var Bayan = {
   UI: {}
 }
 
-
 Bayan.UI.arrayDel = function (btn) {
   $(btn).parents('.bayan-array-element').first().remove()
 }
@@ -21,10 +20,74 @@ Bayan.UI.arrayAdd = function (btn) {
   var addTo = container.find('.bayan-add-array-element').last()
   console.log(html, addTo)
   $(html).attr('class', 'bayan-array-element').insertBefore(addTo)
+  Bayan.processNewElements()
 }
 
-$('.bayan-template').toArray().reverse().forEach(function(tpl) {
-  var innerHTML = tpl.innerHTML;
-  tpl.innerHTML = ''
-  $(tpl).attr('data-template-string', innerHTML)
-})
+Bayan.init = function() {
+  $('.bayan-template').toArray().reverse().forEach(function(tpl) {
+    var innerHTML = tpl.innerHTML;
+    tpl.innerHTML = ''
+    $(tpl).attr('data-template-string', innerHTML)
+  })
+  $(document).ready(Bayan.processNewElements)
+}
+
+Bayan.processNewElements = function() {
+  $('select').material_select();
+
+  $('.autocomplete').each(function(c, elem) {
+    var $elem = $(elem)
+    var options = $elem.attr('data-options')
+    $elem.attr('data-options', null)
+    if (!options) return;
+    options = JSON.parse(options).map(function(opt) {
+      return {
+        id: opt.value,
+        text: opt.label
+      }
+    })
+    console.log(options)
+    $elem.materialize_autocomplete({
+      limit: 20,
+      multiple: {
+          enable: true,
+      },
+      getData: function (value, callback) {
+        // ...
+        // callback(value, data);
+        callback(value, options)
+      }
+    });
+  })
+
+
+  $('.chips-autocomplete').each(function(c, elem) {
+    var $elem = $(elem)
+    var options = $elem.attr('data-options')
+    $elem.attr('data-options', null)
+    var hiddenFieldsContainer = $elem.siblings('.bayan-hidden-fileds')
+    var fieldName = $elem.attr('data-field')
+    if (!options) return;
+    options = JSON.parse(options);
+    console.log(options)
+    $elem.material_chip({
+      autocompleteData: options,
+      secondaryPlaceholder: $elem.attr('data-placeholder') || '+'
+    });
+    $elem.on('chip.add chip.delete', function() {
+      var data = $(this).material_chip('data')
+      hiddenFieldsContainer.innerHTML = ''
+      for (var i = 0; i<data.length; ++i) {
+        var value = data[i].id
+        if (!value) continue
+        $('<input />').attr({
+          type: 'hidden',
+          name: fieldName+'[]',
+          value: value
+        }).appendTo(hiddenFieldsContainer)
+      }
+    })
+  })
+}
+
+Bayan.init()
